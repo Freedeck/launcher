11d586411863436a3469f7017c93be2ca0a3c3a6
ia74 <aidenjs9@icloud.com>
2024-08-18 18:11:00 -0500

    Version bump

--- Form1.cs
+++ Form1.cs
@@ -18,7 +18,7 @@ namespace Freedeck_Launcher
 {
     public partial class Form1 : Form
     {
-        private static string lVersion = "1.4.0";
+        private static string lVersion = "1.5.0";
         public Form1()
         {
             InitializeComponent();
